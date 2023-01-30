c41b63a8946f5e2cfa1ee08f6a6a661db065fba4
Bruno Fernandez <66654095+brunofnz@users.noreply.github.com>
2023-01-30 20:38:08 -0300

    CHANGE float

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -19,7 +19,7 @@ module.exports = (sequelize) => {
       allowNull: false,
     },
     health_score: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.FLOAT,
       allowNull: false,
     },
     steps: {
