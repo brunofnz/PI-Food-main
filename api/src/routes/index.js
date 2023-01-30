1b06d16c2ab9c3a5684b44b96700f62b0d637fcb
Bruno Fernandez <66654095+brunofnz@users.noreply.github.com>
2023-01-29 22:26:01 -0300

    ADD routes seed and get to recipes or diets

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,6 @@
 const { Router } = require('express');
+const { seedDiets, getDiet } = require('../controllers/diet.controller');
+const { seedRecipes, getRecipes } = require('../controllers/recipe.controller');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -8,5 +10,12 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.post('/seedDiets', seedDiets)
+
+router.post('/seedRecipes', seedRecipes)
+
+router.get('/recipes', getRecipes)
+
+router.get('/diets', getDiet)
 
 module.exports = router;
