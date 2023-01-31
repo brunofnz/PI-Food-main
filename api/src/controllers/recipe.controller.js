37d4b62c105443ff9f49bf18887387e2bfdfd39a
Bruno Fernandez <66654095+brunofnz@users.noreply.github.com>
2023-01-31 18:35:39 -0300

    ADD getRecipeById and createRecipe with validations

--- api/src/controllers/recipe.controller.js
+++ api/src/controllers/recipe.controller.js
@@ -129,7 +129,7 @@ const getRecipes = async (req,res) => {
                 }
             }
             if (findRecipe?.length === 0 && results?.length === 0)  res.status(404).send(message(false, `Not found recipe ${title}.`))
-            res.status(200).send(array9)
+            res.status(200).send(messageApi(true, 'Found recipes.',array9))
         } else {
             const { data:{ results } } = await axios.get(`${URL_RECIPES}`)
             const dataApi = await filterRecipeDataApi(results)
@@ -161,7 +161,117 @@ const getRecipes = async (req,res) => {
     }
 }
 
+const getRecipeById = async (req,res) => {
+    try {
+        const regex = /^[0-9(a-f|A-F)]{8}-[0-9(a-f|A-F)]{4}-4[0-9(a-f|A-F)]{3}-[89ab][0-9(a-f|A-F)]{3}-[0-9(a-f|A-F)]{12}$/g;
+        const { idReceta } = req.params;
+        if(idReceta === undefined) res.status(400).send(messageApi(false, 'Bad Request'));
+        const found = idReceta.match(regex);
+        if(found !== null && found.length > 0) {
+            const recipe = await Recipe.findOne({
+                where: { 
+                    id: idReceta 
+                },
+                attributes: [
+                    'id',
+                    'title',
+                    'summary',
+                    'health_score',
+                    'steps',
+                    'image',
+                ],
+                include: {
+                    model: Diet,
+                    attributes: ['name']
+                }
+            })
+            if(recipe) {
+                res.status(200).send(messageApi(true,'Recipe found', recipe));
+            } else {
+                res.status(404).send(messageApi(false,'Recipe not found'));
+            }
+        } else if(found === null){
+            const { data:{ 
+                id,
+                title,
+                summary,
+                healthScore,
+                analyzedInstructions,
+                image,
+                diets 
+            } } = await axios.get(`https://api.spoonacular.com/recipes/${idReceta}/information?apiKey=1ace762ad72d4ee7bc0da59cf3815ccc`);     
+            res.status(200).send(messageApi(true,'Recipe found', { 
+                id,
+                title,
+                summary,
+                health_score: healthScore,
+                steps: analyzedInstructions[0]?.steps,
+                image,
+                diets,
+            }))   
+        } else {
+            res.status(400).send(messageApi(false, 'Bad Request'));
+        }
+    } catch (error) {
+        console.log(`🚀 ~ file: recipe.controller.js:201 ~ getRecipeById ~ error`, error)
+        res.status(error.response.status).send(message(false, error.response.data.detail));
+    }
+}
+
+const createRecipe = async (req,res) => {
+    try {
+        const { 
+            title,
+            summary,
+            image,
+            health_score,
+            steps,
+            diets,         
+        } = req.body;
+        if( 'string' !== typeof title) res.status(400).send(messageApi(false, `Bad Request! 'title' must be string`));
+        if( 'string' !== typeof summary) res.status(400).send(messageApi(false, `Bad Request! 'summary' must be string`));
+        if( 'string' !== typeof image) res.status(400).send(messageApi(false, `Bad Request! 'image' must be string`));
+        if( 'number' !== typeof health_score) res.status(400).send(messageApi(false, `Bad Request! 'health_score' must be string`));
+        if (!Array.isArray(diets)) {
+            res.status(400).send(messageApi(false, `Bad Request! 'diets' must be object[] => [{id: string, name: string}]`));
+        } 
+        if (!Array.isArray(steps)) {
+            res.status(400).send(messageApi(false, `Bad Request! 'steps' must be object[] => [{id: string, name: string}]`));
+        }
+        const recipeValidate = await Recipe.findOne({
+            where: {title}
+        })
+        const dietAsync = async (id) => {
+            let dietFind = await Diet.findByPk(id);
+            return dietFind;
+        }
+        if (!recipeValidate) {
+            const recipeCreate = await Recipe.create({ 
+                title, 
+                summary, 
+                health_score, 
+                steps, 
+                image, 
+                diets,
+            })
+            diets.map(async (diet) => {
+                let dietFinded = await dietAsync(diet.id);
+                if(dietFinded){
+                    recipeCreate.addDiet(diet.id);
+                }
+            })
+            res.status(200).send(messageApi(true, 'Recipe created.', recipeCreate))
+        } else {
+            res.status(417).send(messageApi(false, 'Existing recipe in database.'))
+        }
+    } catch (error) {
+        res.status(400).send(messageApi(false, 'Recipe not created.'))
+    }
+}
+
 module.exports = {
     seedRecipes,
     getRecipes,
+    getRecipeById,
+    createRecipe,
 };
