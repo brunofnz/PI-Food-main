132eff863f731995fa0f005893302ccc08e73745
Bruno Fernandez <66654095+brunofnz@users.noreply.github.com>
2023-01-30 21:07:38 -0300

    ADD filter atributes api

--- api/src/helpers/index.js
+++ api/src/helpers/index.js
@@ -3,6 +3,31 @@ const messageApi = (status, msg, data) => {
     return { status, msg }
 }
 
+const filterRecipeDataApi = async (data) => {
+    // console.log(`🚀 ~ file: index.js:7 ~ filterRecipeDataApi ~ data`, data)
+    if(data === undefined || data === null) return null;
+    return await data?.map(
+        ({ 
+            id,
+            title,
+            summary,
+            healthScore,
+            analyzedInstructions,
+            image,
+            diets 
+        }) => ({
+            id,
+            title,
+            summary,
+            health_score: healthScore,
+            steps: analyzedInstructions[0]?.steps,
+            image,
+            diets,
+        })
+    )
+}
+
 module.exports = {
-    messageApi
+    messageApi,
+    filterRecipeDataApi,
 }
