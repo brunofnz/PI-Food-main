1e6e481d5bbc478b635e2c14e62b4248b2d45a99
Bruno Fernandez <66654095+brunofnz@users.noreply.github.com>
2023-01-31 20:06:23 -0300

    CHANGE landing page

--- client/src/views/LandingPage.js
+++ client/src/views/LandingPage.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import {useHistory} from 'react-router-dom'
+import style from '../assets/styles/LandingPage.module.css'
+import logoRecipe from '../assets/svg/LOGO.svg'
 
-const LandingPage = () => {
+export const LandingPage = () => {
+
+  const history = useHistory();
+
+  function onNavigate(path) {
+    history.push(path)
+  }
+
   return (
-    <div>LandingPage</div>
+    <div className={style.container}>
+      <div className={style.displayLogo}>
+        <div>
+          <img src={logoRecipe} alt="Foods Logo" />
+        </div>
+        <div className={style.presentacionText}>Find the best recipes for your diet or to be happy.</div>
+      </div>
+      <div>
+        <button className={style.buttonStart} onClick={() => onNavigate('/recipes')}>Start</button>
+      </div>
+    </div>
   )
 }
 
-export default LandingPage
+export default LandingPage;
